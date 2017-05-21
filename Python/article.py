1319673a746fdddef2318b88bb30949f93c5e747
Zuko09 <varunknath@gmail.com>
2017-05-20 18:20:46 -0700

    appended tagList Functionality

--- Python/article.py
+++ Python/article.py
@@ -1,23 +1,33 @@
 class article:
-    def __init__(self, articleText):
-        self.tagMap= {'interest' : 0.0}
-        self.Relevance= estimateRelevance(self, articleText)
-        self.articleSource= ""
-        self.textSummary= getSummary(self, articleText)
-        self.timeStamp= getTimeStamp(articleText)
-        getTags(self, articleText)
+    def __init__(self, articleText, articleName):
+        self.dictionary = PyDictionary()
+        self.tagList= []
+        self.articleTitle= articleName
+        self.articleSource= TextBlob(articleText)
+        self.textSummary= self.getSummary(articleText)
+        self.relevance= self.estimateRelevance(articleText)
+        self.timeStamp= self.getTimeStamp()
+        self.getTags(articleText)
 
-    def getTimeStamp(self, articleText):
-        print "TODO finish"
+    def getTimeStamp(articleText):
+        print("TODO finish")
+        
 
-    def estimateRelevance(self, articleText ):
+    def estimateRelevance(self, persona):
         currentRelevance=0.0
-        print "TODO finish"
-        self.Relevance = currentRelevance
+        #print "TODO finish"
+        self.relevance = currentRelevance
 
-    def getSummary(self, articleText ):
-        print "TODO finish"
+    def getSummary(self, articleText):
+        return self.articleSource[0:50]
+
+
 
     def getTags(self, articleText):
-        self.tagMap["IMPLEMENT"] = "ADD MORE STUFF"
-        print "TODO finish"
+        for np in self.articleSource.noun_phrases:
+            self.tagList.append(str(np))
+            for val in (str(np).split()):
+                synList = self.dictionary.synonym(val)
+                if synList == []:
+                    self.tagList.extend(synList)
+        return self.tagList
