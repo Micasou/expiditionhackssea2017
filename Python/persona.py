2b73838100e022017c58219725b181fd9c618663
Alex Orozco <Alex Orozco>
2017-05-20 22:05:36 -0700

    Updated order.

--- Python/persona.py
+++ Python/persona.py
@@ -27,14 +27,14 @@ class persona:
                     for syn in synonymList:
                         self.mappedPreference[syn] = key
                         #print "New key: " + syn
+    def weighArticle(self, countMap):
+        print "TODO improve and use weight relevance to generate the rating."
+        if len(countMap) > 0:
+            return 1
+        return 0
     def personaConsumer(self, articleList):
         matchCountDict = dict()
         for item in articleList:
             if (self.mappedPreference.get(item, "empty")!= "empty"):
                 matchCountDict[self.mappedPreference[syn]] += 1
         return weighArticle(matchCountDict)
-    def weighArticle(self, countMap):
-        print "TODO improve and use weight relevance to generate the rating."
-        if len(countMap) > 0:
-            return 1
-        return 0
